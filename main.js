3bbf1e4a797418f264f562fb39f0f91a664c2e17
mauroindigne <mauroindigne@gmail.com>
2020-03-05 11:48:30 +0000

    inital commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const baseURL = "https://swapi.co/api/";
+
+function getData(cb) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
+
+function writeToDocument(type) {
+    getData(type, function(data;) {
+        document.getElementById("data").innerHTML = data;
+
+    });
+}
